dba43c94a96636a0c9c4f8f5019d826bbe31e06e
LinkSake <angel-ortega12@hotmail.com>
2018-06-22 18:39:49 -0600

    Se agrega los poligonos de los distritos a nivel federal, así como se completa el template de los candiatos, faltando solo las imagenes

---
+++ js/districtConvination.js
@@ -0,0 +1,49 @@
+function checkConvitation (dL, dF){
+
+    if (dF == 6 ) {
+        switch (dL) {
+            case 12:
+                
+                break;
+            case 15:
+                
+                break;
+            case 16:
+                
+                break;
+            case 17:
+                
+                break;
+            case 18:
+                
+                break;
+            default:
+                break;
+        }
+    }
+
+    if (dF == 8) {
+        switch (dL) {
+            case 12:
+                
+                break;
+            case 15:
+                
+                break;
+            case 16:
+                
+                break;
+            case 17:
+                
+                break;
+            case 18:
+                
+                break;
+            default:
+                break;
+        }
+    } else {
+
+    }
+
+}
