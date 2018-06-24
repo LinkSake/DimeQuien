2454ec6280364be6952e4e25d8c7aa8b66b681c8
LinkSake <angel-ortega12@hotmail.com>
2018-06-24 14:19:20 -0600

    Se añaden la fotos de los candidatos precidenciales, el smooth scroll en template.html y el botón que redijirá a la página correspondiente según los distritos

--- js/districtCombination.js
+++ js/districtCombination.js
@@ -1,21 +1,23 @@
-function checkConvitation (dL, dF){
+function checkCombination (dL, dF){
 
     if (dF == 6 ) {
         switch (dL) {
             case 12:
-                
+                return 'distrits/6_12.html';
                 break;
             case 15:
-                
+                //window.location.href = 'districts/template.html';
+                return 'districts/template.html';
+                //return 'distrits/6_15.html';
                 break;
             case 16:
-                
+                return 'distrits/6_16.html';
                 break;
             case 17:
-                
+                return 'distrits/6_17.html';
                 break;
             case 18:
-                
+                return 'distrits/6_18.html';
                 break;
             default:
                 break;
@@ -25,25 +27,22 @@ function checkConvitation (dL, dF){
     if (dF == 8) {
         switch (dL) {
             case 12:
-                
+                return 'distrits/8_12.html';
                 break;
             case 15:
-                
+                return 'distrits/8_15.html';
                 break;
             case 16:
-                
+                return 'distrits/8_16.html';
                 break;
             case 17:
-                
+                return 'distrits/8_17.html';
                 break;
             case 18:
-                
+                return 'distrits/8_18.html';
                 break;
             default:
                 break;
         }
-    } else {
-
-    }
-
+    } 
 }
